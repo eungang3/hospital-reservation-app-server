eae2cdb833d2c7200cf6bd02bb620563b91aba7e
azure928 <azure6.dev@gmail.com>
2022-10-15 20:03:09 +0900

    Chore: 사용하지 않는 console.log 제거

--- services/reservationService.js
+++ services/reservationService.js
@@ -1,16 +1,14 @@
 const reservationDao = require('../models/reservationDao');
 const { v4 } = require('uuid');
 
-const createReservation = async (data) => {
-  const { type, user_name, phone_number, email, hospital_id, time_window_id } = data;
+const createReservation = async (body) => {
+  const { type, user_name, phone_number, email, hospital_id, time_window_id } = body;
   let patient_id;
   let reservation_number = v4();
 
   // 이미 예약된 시간인지 확인
   const seletedReservation = await reservationDao.readReservation(hospital_id, time_window_id);
-  //console.log('seletedReservation!!!', seletedReservation.length);
   if (seletedReservation.length > 0) {
-    //console.log('이미 예약된 시간임');
     const error = new Error('already reserved time');
     error.statusCode = 409;
     throw error;
@@ -18,21 +16,17 @@ const createReservation = async (data) => {
 
   //patients 테이블에 등록되어있는 환자인지 확인
   const seletedPatient = await reservationDao.readPatientIdByPhoneNumber(phone_number);
-  //console.log('seletedPatients.length', seletedPatients.length);
 
   if (seletedPatient.length > 0) {
     //등록된 환자인 경우
-    //console.log('등록된 환자');
-    //console.log('seletedPatients', seletedPatients[0].id);
     patient_id = seletedPatient[0].id;
   } else {
     //등록되지 않은 환자인 경우
-    //console.log('등록되지 않은 환자');
     const createdPatient = await reservationDao.createPatient(user_name, phone_number, email);
-    //console.log('createdPatient.insertId!!!', createdPatient.insertId);
     patient_id = createdPatient.insertId;
   }
 
+  //예약 등록
   const createdReservation = await reservationDao.createReservation(
     type,
     reservation_number,
@@ -40,7 +34,6 @@ const createReservation = async (data) => {
     hospital_id,
     time_window_id
   );
-  //console.log('createdReservation', createdReservation);
 };
 
 module.exports = { createReservation };

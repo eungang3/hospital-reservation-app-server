12d01db454a7d8f02b9cd183dc3d293382244a01
Ji Yook <azure6.dev@gmail.com>
2022-10-16 13:16:28 +0900

    Revert "Add: 예약정보수정"

--- services/reservationService.js
+++ services/reservationService.js
@@ -64,29 +64,4 @@ const updateReservationStatus = async (reservation_id, status) => {
   await reservationDao.updateReservationStatus(status, reservation_id);
 };
 
-const updateReservationService = async(reservationDto) => {
-  const originInfo = await reservationDao.findReservationInfo(reservationDto.reservation_number);
-  reservationDto['originInfo'] = originInfo;
-
-  if(originInfo){
-      if(reservationDto.name){
-          await reservationDao.updateName(reservationDto);
-      }
-      if(reservationDto.time_window_id){
-          await reservationDao.updateTime(reservationDto);
-      }
-      if(reservationDto.type){
-          await reservationDao.updateType(reservationDto);
-      }
-  }else{
-      const error = new Error("check reservation_number");
-      error.statusCode = 400;
-      throw error;
-  }
-
-  const reservationInfo = await reservationDao.findReservationInfo(reservationDto.reservation_number);
-
-  return reservationInfo;
-};
-
-module.exports = { createReservation, updateReservationStatus, updateReservationService };
+module.exports = { createReservation, updateReservationStatus };

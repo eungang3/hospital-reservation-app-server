68e26c488f494f82e1cd0f04b759bcde2d8f1949
enddl3224 <enddl3224@naver.com>
2022-10-16 11:25:30 +0900

    Add: 예약정보수정

--- routes/reservationRouter.js
+++ routes/reservationRouter.js
@@ -5,5 +5,6 @@ const router = express.Router();
 
 router.post('/', reservationController.createReservation);
 router.patch('/:id', reservationController.updateReservationStatus);
+router.patch('/update/info', reservationController.updateReservationController);
 
 module.exports = router;
